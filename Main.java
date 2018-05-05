273e5c75b070b21421b1c3161425abc41762668c
vietthangngo <viettkhangngo@gmail.com>
2018-05-05 12:55:32 +0300

    1

--- Main.java
+++ Main.java
@@ -3,7 +3,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hi");
-        System.out.println("hello world !!!");
+        SayHello.sayHello();
+        reverseArgs.reverseArgs();
+        line.Line();
+        validatePassword.validatePassword();
+        printSum.printSum();
+        Arrays.taskSix();
+        Arrays.taskSix();
+
     }
 }
